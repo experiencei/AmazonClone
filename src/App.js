7d737f02b1d28302a2764480cb9da4d92967faca
experiencei <ayelojahighbee01@gmail.com>
2021-10-10 16:44:35 +0100

    implementing react-router-dom and checkout pages

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
 
+import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
+import Checkout from './components/checkout/Checkout';
 function App() {
   return (
+    <Router>
     <div className="app">
-     <Header/>
-     <Home/>
+    <Switch>
+       
+        <Route path="/checkout">
+            <Header/>
+            <Checkout/>
+        </Route>
+        <Route path="/">
+            <Header/>
+            <Home />  
+        </Route>
+     </Switch>
     </div>
+    </Router>
   );
 }
 
