88ab0f4198c46da52d5d1c4746f0c66772168ba8
experiencei <ayelojahighbee01@gmail.com>
2021-10-12 00:36:14 +0100

    sepreting the front end from the backend server

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,22 @@
 const functions = require("firebase-functions");
+const  express = require("express");
+const cors = require("cors");
+const stripe = require("stripe")("sk_test_51JZ1ycKGIJkkC9mHxZvAxIESn1m2Bj8nrWCiUQ7BpYnEisjO94TU5hBr1PDm6AYduzECSzBYDncYnsSj6nnM1jYY00AuWesXIy");
 
+//API
+
+//App Config
+const app = express();
+
+//Middlewares
+app.use(cors({origin: true}));
+app.use(express.json());
+
+// - API routes
+app.get("/"  , (req , res) => res.status(200).send(
+    'hello world'
+))
+
+// -Listen command
+exports.api = functions.https.onRequest(app);
 
