9604f61c001449cfc66fc8021029d6d03a8e0bee
experiencei <ayelojahighbee01@gmail.com>
2021-10-12 10:05:57 +0100

    impementing google firestore database

--- functions/index.js
+++ functions/index.js
@@ -13,10 +13,22 @@ app.use(cors({origin: true}));
 app.use(express.json());
 
 // - API routes
-app.get("/"  , (req , res) => res.status(200).send(
+app.get("/"  , ( request , response) => response.status(200).send(
     'hello world'
 ))
+app.post("/payments/create" , async ( request , response) =>  {
+    const total = request.query.total;
 
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount : total,
+        currency : "usd"
+    });
+
+    response.status(201).send({
+        clientSecret : paymentIntent.client_secret,
+    })
+})
 // -Listen command
 exports.api = functions.https.onRequest(app);
 
+// http://localhost:5001/db-54246/us-central1/api
